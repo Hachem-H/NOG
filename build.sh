7cb12d5b9710f222b9d3c9221050f5f95be5c133
hh-Naram <hh.naram@gmail.com>
2022-08-02 21:09:38 +0300

    Add breakout to script

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ mkdir -p images
 cargo bootimage
 
 cp target/target/debug/bootimage-tictactoe.bin images/TicTacToe.bin
+cp target/target/debug/bootimage-breakout.bin images/Breakout.bin
 cp target/target/debug/bootimage-pong.bin images/Pong.bin
